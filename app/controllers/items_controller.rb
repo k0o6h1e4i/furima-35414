4ca5dcffd3e4eb61322325d79907c318b1e8ff93
k0o6h1e4i <82030550+k0o6h1e4i@users.noreply.github.com>
2021-05-04 15:01:33 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,17 @@ class ItemsController < ApplicationController
  end
 
  def new
-  @items = Item.all
+  @item = Item.all
  end
+
+ def create
+  @item =Item.new
+ end
+
+
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :text, :price, :state_id, :category_id, :postage_id, :prefecture_id, :shipping_day_id, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,19 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :buyer
+  has_one_attached :image
+
+  with_options presence:true do
+    validates :title, presence: true
+    validates :text, presence: true 
+    validates :price,presence: true
+    validates :state_id, presence: true
+    validates :category_id, presence: true
+    validates :postage_id, presence: true
+    validates :prefecture_id, presence: true
+    validates :shipping_day_id, presence: true
+    
+   end   
+
+
 end

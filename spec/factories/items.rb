30370d2b0a2e552813d7a3742b3ed0aa8810030b
k0o6h1e4i <82030550+k0o6h1e4i@users.noreply.github.com>
2021-05-04 22:58:58 +0900

    インデントの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,19 +1,17 @@
 FactoryBot.define do
   factory :item do
     title              {'スニーカー '}
-    text                 {'特に目立った傷や汚れ等なく、まだまだ使っていただけると思います。'}
+    text               {'特に目立った傷や汚れ等なく、まだまだ使っていただけると思います。'}
     price              {98765}
-    state_id             {2}
-    category_id            {3}
-    postage_id             {3}
-    prefecture_id            {3}
-    shipping_day_id              {3}
+    state_id           {2}
+    category_id        {3}
+    postage_id         {3}
+    prefecture_id      {3}
+    shipping_day_id    {3}
     association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
-
-
 end

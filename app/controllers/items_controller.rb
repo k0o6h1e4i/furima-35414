fc2d0ab0e1d2d844d6970f2f4755ea881eaaea56
k0o6h1e4i <82030550+k0o6h1e4i@users.noreply.github.com>
2021-05-07 12:21:25 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create, :edit, :update, :destroy]
-  before_action :find_item, only:[:show, :edit, :update]
+  before_action :find_item, only:[:show, :edit, :update,:destroy]
   before_action :require_item_user, only:[:edit, :update]
 
  def index
@@ -18,8 +18,7 @@ class ItemsController < ApplicationController
  end
 
  def destroy
-    item = Item.find(params[:id])
-     item.destroy
+     @item.destroy
      redirect_to root_path
     
  end
